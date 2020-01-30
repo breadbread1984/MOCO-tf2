c9d9dcc5a5eb80cf6d0697776ca936b536aa147e
breadbread1984 <breadbread1984@163.com>
2020-01-30 10:45:23 +0800

    update train

--- train.py
+++ train.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 
 import tensorflow as tf;
+import tensorflow_datasets as tfds;
 from models import Encoder, ImgQueue;
 from download_dataset import parse_function;
 
+batch_size = 100;
+
 def main():
 
-  
+  model = Encoder((64,64,3));
+  optimizer = tf.keras.optimizers.Adam(0.01, decay = 0.0001);
+  trainset = iter(tfds.load(name = 'imagenet_resized/64x64', split = tfds.Split.TRAIN, download = False).repeat(-1).map(parse_function).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE));
+  # restore from existing checkpoint
+  checkpoint = tf.train.Checkpoint(model = model, optimizer = optimizer);
+  checkpoint.restore(tf.train.latest_checkpoint('checkpoints'));
+  # create log
+  log = tf.summary.create_file_writer('checkpoints');
+  # metrics
+  avg_loss = tf.keras.metrics.Mean(name = 'loss', dtype = tf.float32);
+  while True:
+    image, _ = next(trainset);
+    
 
 if __name__ == "__main__":
 
