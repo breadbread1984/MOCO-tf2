9c15485367a469e22b0480665ed9c82420f3bc15
breadbread1984 <breadbread1984@163.com>
2020-01-31 11:08:58 +0800

    update model

--- models.py
+++ models.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
   from download_dataset import parse_function;
   trainset = iter(tfds.load(name = 'imagenet_resized/64x64', split = tfds.Split.TRAIN, download = False).map(parse_function).batch(8));
   images, _ = next(trainset);
-  augments = RandomAugmentation(image.shape[-3:], rotation_range = (-10, 10))(images);
+  augments = RandomAugmentation(images.shape[-3:], rotation_range = (-10, 10))(images);
   for i in range(8):
     image = tf.cast(tf.clip_by_value(images[i, ...] * 255., 0., 255.), dtype = tf.uint8);
     augment = tf.cast(tf.clip_by_value((augments[i, ...] + 1) * 127.5, 0., 255.), dtype = tf.uint8);
